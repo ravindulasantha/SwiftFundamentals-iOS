34d5ec6d59a80e61384cbc1490a29486b76e59a9
Ravindu Lasantha <67038119+ravindulasantha@users.noreply.github.com>
2023-04-01 10:46:33 +0530

    control flow with swift

---
+++ SwiftFundarmentals.playground/Pages/Branching If Switch.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+
+var wether:String
+
+let sunny:String = "sunny"
+
+let wet:String = "wet"
+ 
+let windy:String = "windy"
+
+wether = windy
+
+if wether == sunny{
+    print("Wear Sunny Clothes")
+}else if wether == windy{
+    print("Wear Windy Clothes")
+}
